0956563284dc378bb1b42f3339f7e3a47667af1e
Suvargha Ghosh Dastidar <suvargha2000@gmail.com>
2023-05-18 23:31:39 +0530

    made into single file

--- main.py
+++ main.py
@@ -1,15 +1,57 @@
 from flask import Flask , request
-import query
+import sqlite3
+from datetime import datetime 
+
+class sqliteHandle:
+
+    def __init__(self) -> None:
+        self.conn = sqlite3.connect('test.db',check_same_thread=False)
+        self.cur = self.conn.cursor()
+        self.__checktable()
+
+
+    def __createTable(self):
+        self.conn.execute('''CREATE TABLE data
+         (time TIMESTAMP PRIMARY KEY     NOT NULL,
+         temp           INT    NOT NULL,
+         moisture            INT     NOT NULL,
+         smoisture         INT      NOT NULL);''')
+        
+    def __checktable(self):
+        try:
+            self.cur.execute("SELECT * FROM data")
+        except : 
+            self.__createTable()
+
+    def addData(self , dicto : dict) : 
+        records =  [datetime.now()]
+        for keys in dicto : 
+            records.append(dicto[keys])
+
+        self.cur.execute("INSERT INTO data VALUES (?,?,?,?)", records)
+        self.conn.commit()
+        print(dicto)
+
+    def retData(self , dates):
+        data = self.cur.execute("SELECT * FROM data where time between DATE(?) and DATE(?)" , (dates[0] , dates[1]))
+        return data
 
 app = Flask("__name__")
-connection = query.sqliteHandle()
+connection = sqliteHandle()
 
 @app.route("/retData", methods = ['POST'])
 def retrieveData():
     if request.method != "POST" : 
         return "Not Accepted"
 
-    return "Data"
+    data = [request.get_json()["sDate"] , request.get_json()["eDate"]]
+    
+    data = connection.retData(data)
+    x = []
+    for i in data : 
+        x.append(i)
+
+    return x
 
 @app.route("/postData" , methods = ['POST'])
 def postData():
